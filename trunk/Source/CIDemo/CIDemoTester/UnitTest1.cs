e449cc6a90f6bd4811e7c92c572d256b746971fb
farinel569 <bloodedgedreaper@gmail.com>
2015-03-12 09:44:45 -0400

    test

---
+++ trunk/Source/CIDemo/CIDemoTester/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace CIDemoTester
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        { 
+            var result = CIDemo.Program.Main(new[] {"foo","foo"});
+            Assert.AreEqual(1,result);
+       
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var result = CIDemo.Program.Main(new[] {"fooo","foo"});
+            Assert.AreEqual(0,result);
+
+        }
+
+    }
+}
---
+++ trunk/Source/CIDemo/CIMain/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIDemo
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            return args[0] == args[1] ? 1 : 0;
+            Console.WriteLine(null);
+        }
+    }
+}
